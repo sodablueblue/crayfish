32d05c8a4d7777afb02e511449e34de589dca659
Liu, Zheng <zheng.l.liu@intel.com>
2016-03-17 14:34:34 +0800

    Charts add

--- public/js/main.js
+++ public/js/main.js
@@ -4,17 +4,69 @@ $(function(){
 			xiaoqu: $('#xiaoqu').val(),
 			test: 'test'
 		}, function (data, textStatus){
-			$('#resText').empty()
-			$(data).each(function(index, item){
-				var date = new Date(item.created * 1000);
-				$('#resText').prepend(
-					'<p>' + decodeURI(item.community) + '</p>' + 
-					'<p>' + item.meanPrice + '元</p>' +
-					'<p>' + date.toLocaleString() + '</p>' +
-					'<hr/>');
+			// $('#resText').empty()
+			// $(data).each(function(index, item){
+			// 	var date = new Date(item.created * 1000);
+			// 	$('#resText').prepend(
+			// 		'<p>' + decodeURI(item.community) + '</p>' + 
+			// 		'<p>' + item.meanPrice + '元</p>' +
+			// 		'<p>' + date.toLocaleString() + '</p>' +
+			// 		'<hr/>');
 
-			});
+			// });
+			var conv = convertData(data);
+			$("#resText").empty();
+			$("#resText").highcharts({
+				chart: {
+					type: 'line'
+				},
+				title: {
+					text: decodeURIComponent(conv.community) + '均价趋势'
+				},
+				xAxis: {
+					type: 'datetime',
+					dateTimeLabelFormats: {
+						day: '$e of $b'
+					}
+				},
+				yAxis: {
+					title: {
+						text: '元'
+					}
+				},
+				series:[{
+					name: '链家',
+					data: conv.data,
+					pointStart: conv.pointStart * 1000,
+					pointInterval: 12 * 3600 * 1000
+				}]
+			})
 		});
 		return false;
 	});
 });
+
+function compare(a, b){
+	return a.created - b.created
+}
+
+function convertData(data){
+	var res = {
+		community: '',
+		data: [],
+		pointStart: ''
+	};
+
+	data = data.sort(compare);
+
+	$(data).each(function(index, item){
+		if(index == 0) {
+			res.community = item.community;
+			res.data.push(Number(item.meanPrice));
+			res.pointStart = item.created;
+		}else{
+				res.data.push(Number(item.meanPrice));
+		}
+	});
+	return res;
+}
